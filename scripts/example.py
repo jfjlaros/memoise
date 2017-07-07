d65c84bfc48de53916e8fc91def2bf3b83938486
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2017-07-07 15:45:21 +0200

    Pep8.

--- scripts/example.py
+++ scripts/example.py
@@ -2,18 +2,22 @@
 
 from memoise.memoise import Cache
 
+
 @Cache()
 def add(a, b, x=1, opt=0):
     return a + b + x + opt
+
 
 @Cache(timeout=10)
 def subst(a, b):
     return a - b
 
+
 class Test(object):
     @Cache(ignore=[0, "b"])
     def something(self, x, y, a=1, b=2):
         return x + y
+
 
 print add(12, 4, x=1, opt=1)
 print add(12, 4, opt=1, x=1)
